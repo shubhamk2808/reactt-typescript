1f6686ab6afc20471279044c4bdbc48f0d1c5342
shubham <shubhamk2808@gmail.com>
2022-10-09 14:50:32 +0530

    last commit

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,11 +1,14 @@
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Box } from '@mui/material';
-import { useEffect } from 'react';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Box, Button, Typography, Backdrop } from '@mui/material';
+import { useState, useEffect } from 'react';
 import { styled } from '@mui/system';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, AppDispatch } from '../app/store';
 import { getUserData, getSortedData } from '../redux/actions/users';
+import reactlogo from "../assets/react.svg";
+import ClearIcon from '@mui/icons-material/Clear';
+import CircularProgress from '@mui/material/CircularProgress';
 
 interface IUser {
     id: number;
@@ -18,26 +21,34 @@ interface IUser {
 }
 
 const Users = () => {
+    const [showClear, setShowClear] = useState<boolean>(false);
     const userState = useSelector((state: RootState) => state.userRedcers.users);
+    const { loading } = useSelector((state: RootState) => state.userRedcers);
     const dispatch = useDispatch<AppDispatch>();
-  
 
-    useEffect(() => {
-       dispatch(getUserData())
-    }, [dispatch])
-    
-    const handleSort = (sortBy:string) => {
+    const handleGetUser = () => {
+        dispatch(getUserData());
+        setShowClear(false);
+    }
+
+    const handleSort = (sortBy: string) => {
         let sortByTime: any = [...userState]
         sortByTime.sort(function (a: any, b: any) {
-            let x:any = new Date('1970/01/01 ' + a.time)
-            let y:any = new Date('1970/01/01 ' + b.time)
-            return  sortBy === 'AMPM' ? x - y : sortBy === 'PMAM' && y - x;
+            let x: any = new Date('1970/01/01 ' + a.time)
+            let y: any = new Date('1970/01/01 ' + b.time)
+            return sortBy === 'AMPM' ? x - y : sortBy === 'PMAM' && y - x;
         });
         dispatch(getSortedData(sortByTime))
+        setShowClear(true)
     }
+
     return (
         <>
             <DIV>
+                <Span>
+                    <Typography color="#292D32" variant='h4' textAlign="center">User Details Table</Typography>
+                    <Button style={{ display: 'flex', alignItems: 'end', justifyContent: 'center', fontWeight:'600' }} variant="outlined" disabled={userState.length > 0} onClick={() => handleGetUser()}>Get Users</Button>
+                </Span>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} stickyHeader aria-label="simple table">
                         <TableHead>
@@ -52,15 +63,20 @@ const Users = () => {
                                     <Flex>
                                         <div>Time</div>
                                         <div>
-                                            <div><ArrowDropUpIcon style={{ color: "grey" }} onClick={()=>handleSort('AMPM')} /></div>
-                                            <div><ArrowDropDownIcon style={{ color: "grey" }} onClick={()=>handleSort('PMAM')} /></div>
+                                            <div><ArrowDropUpIcon style={{ color: "grey" }} onClick={() => handleSort('AMPM')} /></div>
+                                            <div><ArrowDropDownIcon style={{ color: "grey" }} onClick={() => handleSort('PMAM')} /></div>
                                         </div>
+                                        {showClear &&
+                                            <div>
+                                                <div><ClearIcon style={{ color: "grey" }} onClick={() => handleGetUser()} /></div>
+                                            </div>
+                                        }
                                     </Flex>
                                 </Header>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {userState && userState.map((user: IUser) => {
+                            {userState && userState.length > 0 ? userState.map((user: IUser) => {
                                 return (
                                     <TableRow
                                         key={user.id}
@@ -75,11 +91,28 @@ const Users = () => {
                                         <TableCell align="left">{user.time}</TableCell>
                                     </TableRow>
                                 )
-                            })}
+                            }) :
+                                (<TableRow style={{ height: '400px' }}>
+                                    <TableCell colSpan={7}>
+                                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                            <img width="50px" src={reactlogo} style={{
+                                                color: "grey", objectFit: 'contain',
+                                            }} />
+                                        </div>
+                                        <Typography align="center" style={{ lineHeight: '3.5rem' }}>No Users found</Typography></TableCell>
+                                </TableRow>)
+
+                            }
                         </TableBody>
                     </Table>
                 </TableContainer>
             </DIV>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={loading}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
         </>
     )
 }
@@ -87,12 +120,15 @@ const Users = () => {
 export default Users
 
 const DIV = styled(Box)({
-    // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     background: '#F2F2F2',
     border: 0,
     borderRadius: 1,
     color: 'white',
-    padding: '100px 100px',
+    padding: '50px 50px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    gap: '10px',
     '& > div': {
         borderRadius: '20px',
         boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .1)',
@@ -110,3 +146,11 @@ const Flex = styled('div')({
     justifyContent: 'center',
     alignItems: 'center',
 });
+
+const Span = styled('span')({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '-webkit-fill-available',
+    padding: '5px 15px',
+})
--- src/utils/counterAPI.ts
+++ src/utils/counterAPI.ts
@@ -3,6 +3,6 @@ import {userData} from './MOCK_DATA'
 
 export function fetchUsers() {
   return new Promise<{ data: any }>((resolve) =>
-    setTimeout(() => resolve({ data: userData }), 5000)
+    setTimeout(() => resolve({ data: userData }), 2000)
   );
 }
